4865e6581bfcae4723f87122e044095b9a802b01
mhendricks96 <mhendricks96@yahoo.com>
2021-04-24 15:41:26 -0700

    copleted 'ab 11

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
+
   <React.StrictMode>
+    <Auth0Provider
+    domain="bestbooks.us.auth0.com"
+    clientId="N1HNOVKUSlqb1bGmZATLwKxDInOL6e8y"
+    redirectUri={window.location.origin}
+  >
     <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
