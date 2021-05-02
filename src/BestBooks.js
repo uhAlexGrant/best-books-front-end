aa81532eed7deec18fa0cbbb09b2fc54d05d7c88
mhendricks96 <mhendricks96@yahoo.com>
2021-05-02 15:18:57 -0700

    trying to figure out modal

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -3,8 +3,6 @@ import './App.css';
 import axios from 'axios';
 //import { getMaxListeners } from 'superagent';
 import { withAuth0 } from '@auth0/auth0-react';
-//import Modal from 'react-bootstrap/Modal';
-import BookFormModal from './BookFormModal.js'
 
 
 
@@ -32,11 +30,11 @@ class BestBooks extends React.Component {
 
   handleCreateBook = (e) => {
     e.preventDefault();
-    const { user } = this.props.auth0;
     this.setState( {
      description: this.state.description,
      name: this.state.name,
    })
+    const { user } = this.props.auth0;
     axios.post(`${process.env.REACT_APP_BACKEND_URL}/book?user=${user.email}`
      )
     .then(response => console.log(response.data));
@@ -59,7 +57,16 @@ class BestBooks extends React.Component {
 
 
 
-        < BookFormModal handleCreateBook={this.handleCreateBook} saveBookName={this.saveBookName} saveBookDescription={this.saveBookDescription}/>
+        <form onSubmit={this.handleCreateBook}>
+          <label htmlFor="bookName">Name</label>
+          <input id="bookName" type="text" onInput={this.saveBookName}></input>
+          <br />
+          <label htmlFor="description">Description</label>
+          <input id ="description" type="text" onInput={this.saveBookDescription}></input>
+          <br />
+          <input type="submit" />
+        </form>
+
         <h1>books</h1>
         {this.state.books && this.state.books.map(book => <h3 key={book._id}>{book.bookName}</h3>)}
       </div>
--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -2,50 +2,31 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
 
 
-  function Example() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  }
-  render(){
-    return (
-      <>
-          <Button variant="primary" onClick={handleShow}>
-      Launch demo modal
-      </Button>
-
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <form onSubmit={this.props.handleCreateBook}>
-                <label htmlFor="bookName">Name</label>
-                <input id="bookName" type="text" onInput={this.saveBookName}></input>
-                <br />
-                <label htmlFor="description">Description</label>
-                <input id="description" type="text" onInput={this.saveBookDescription}></input>
-                <br />
-              </form>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-          Close
-          </Button>
-              <Button variant="primary" onClick={handleClose}>
-          Save Changes
-          </Button>
-            </Modal.Footer>
-          </Modal>
-        </>
   
-    )
-  }
+class BookFormModal extends React.Component {
 
+  render() {
+    return(
+      <Modal.Dialog>
+  <Modal.Header closeButton>
+    <Modal.Title>Modal title</Modal.Title>
+  </Modal.Header>
+
+  <Modal.Body>
+    <p>Modal body text goes here.</p>
+  </Modal.Body>
+
+  <Modal.Footer>
+    <Button variant="secondary">Close</Button>
+    <Button variant="primary">Save changes</Button>
+  </Modal.Footer>
+</Modal.Dialog>
+    );
+  }
+}
 
 
 export default BookFormModal;
